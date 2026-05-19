8e6bd9057582424aca7c8cdfb4a2d0cef1c3ef07
RaphaelRoyM <raphaelroym@gmail.com>
2026-05-19 20:24:47 +0530

    delete functionality completed. marking button completed

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -9,17 +9,33 @@ function App() {
   function addTodo() {
 
     if (task.trim() === "") return;//if trim is not used then " " empty space is considered as a task and it will be added to the list of todos
-    setTodos([...todos, task]);
+    setTodos([...todos,
+    {
+      text: task,
+      completed: false
+    }
+    ]);
     setTask("");
   }
 
-  function deleteTodo(index) {
-    const updatedTodos = todos.filter((todos, index) => {//why splice is not used because splice mutates the original array and filter returns a new array without mutating the original array
-      return index !== indexToDelete
+  function deleteTodo(indexToDelete) {
+    const updatedTodos = todos.filter((todo, index) => {//why splice is not used because splice mutates the original array and filter returns a new array without mutating the original array
+      return index !== indexToDelete;
     });
     setTodos(updatedTodos);
   }
 
+  function toggleComplete(indexToToggle) {
+    const updatedTodos = todos.map((todo, index) => {
+      if (index === indexToToggle) {
+        return {
+          ...todo,
+          completed: !todo.completed
+        };
+      }
+    });
+    setTodos(updatedTodos);
+  }
   return (
     <div>
       <h1>
@@ -34,10 +50,21 @@ function App() {
       <ul>
         {
           todos.map((todo, index) => (
-            <li key={index} >{todo}
+            <li key={index} >
+              <span>
+                {
+                  todo.completed ? "✅" : "❌"
+                }
+                {todo.text}
+              </span>
+
               <button onClick={() => deleteTodo(index)}>
                 Delete
               </button>
+              <button onClick={() => toggleComplete(index)}>
+                Mark Completed
+              </button>
+
             </li>
           ))
         }
