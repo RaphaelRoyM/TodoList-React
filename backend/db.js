0e8a82f009662093e318d46b4288b762a07aad0a
RaphaelRoyM <raphaelroym@gmail.com>
2026-05-27 15:35:28 +0530

    began db connection. loads data from db when loads and refresh. can add new to do to database successfully

---
+++ backend/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "todo_app"
+});
+
+connection.connect((err) => {
+
+  if (err) {
+    console.log("Database connection failed");
+    console.log(err);
+    return;
+  }
+
+  console.log("MySQL Connected");
+
+});
+
+module.exports = connection;
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();// create an instance of express application
 app.use(cors());// use cors middleware to allow cross-origin requests from the frontend application. This is necessary because the frontend and backend are running on different ports (frontend on port 3000 and backend on port 5000). Without this, the browser will block the requests from the frontend to the backend due to CORS policy.cross-origin resource sharing (CORS)
 app.use(express.json());
+const db = require("./db");
 
 // we can use an array to store the todos in memory. This is just for demonstration purposes. In a real application, you would use a database to store the todos.
 let todos = [
@@ -23,18 +24,38 @@ app.get("/", (req, res) => {
 });
 
 app.get("/todos", (req, res) => {
-  res.json(todos);// send the todos as a JSON response. if we use res.send(todos) then it will send the todos as a string and it will not be parsed as a JSON object in the client side. so we need to use res.json(todos) to send the todos as a JSON response.
-  //in fronted we can use fetch("/todos") to get the todos from the server and then we can use response.json() to parse the response as a JSON object and then we can use the todos in our app.
+  // res.json(todos);// send the todos as a JSON response. if we use res.send(todos) then it will send the todos as a string and it will not be parsed as a JSON object in the client side. so we need to use res.json(todos) to send the todos as a JSON response.
+  // //in fronted we can use fetch("/todos") to get the todos from the server and then we can use response.json() to parse the response as a JSON object and then we can use the todos in our app.
+
+  const sql = "SELECT * FROM todos";
+  db.query(sql, (err, result) => {
+    if (err) {
+      // console.log("Error fetching todos from database");
+      console.log(err);
+      res.status(500).json({
+        error: "Failed to fetch todos from database"
+      });
+    }
+    res.json(result);
+  });
 });
 
 app.post("/todos", (req, res) => {
-  const newTodo = {
-    id: Date.now(),// generate a unique id for the new todo using the current timestamp. This is just for demonstration purposes. In a real application, you would use a database to generate unique ids for the todos.
-    text: req.body.text,
-    completed: false
-  };
-  todos.push(newTodo);
-  res.json(newTodo);
+  const { text } = req.body;
+  const sql = `INSERT INTO todos (text,completed) VALUES (?,?)`;
+  db.query(sql, [text, false], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: "Failed to add todo to database"
+      });
+      return;
+    }
+    res.json({
+      message: "Todo added successfully",
+      todoId: result.insertId
+    });
+  });
 });
 
 
@@ -70,21 +91,6 @@ app.put("/todos/:id", (req, res) => {
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 url = app.listen(5000, () => {
   console.log("server is running on port 5000");
 });// start the server and listen on port 5000
-
